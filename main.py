4146461a4fcb67fdae7b5a004e76aa90467e48cd
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-04-09 10:48:45 -0600

    Implements get_discussions

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 from fastapi import FastAPI
 
 from models import Course
+from models import Discussion
 
 # /api/v1/courses/:course_id/discussion_topics
 
@@ -22,9 +23,28 @@ headers: dict[str, str] = {
 
 
 @app.get("/courses")
-async def get_courses() -> Course:
+async def get_courses() -> list[Course]:
     response = requests.get(url=f"{base_url}/courses", headers=headers)
     r_json = response.json()
 
-    course = Course(id=r_json[0]["id"], name=r_json[0]["name"])
-    return course
+    courses: list[Course] = []
+    for course_json in r_json:
+        course = Course(id=course_json["id"], name=course_json["name"])
+        courses.append(course)
+
+    return courses
+
+
+@app.get("/discussions")
+async def get_discussions(course_id: int) -> list:
+    response = requests.get(
+        url=f"{base_url}/courses/{course_id}/discussion_topics", headers=headers)
+    r_json = response.json()
+
+    discussions: list[Discussion] = []
+    for discussion_json in r_json:
+        discussion = Discussion(
+            id=discussion_json["id"], title=discussion_json["title"])
+        discussions.append(discussion)
+
+    return discussions
--- models.py
+++ models.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel
 class Course(BaseModel):
     id: int
     name: str
+
+
+class Discussion(BaseModel):
+    id: int
+    title: str
