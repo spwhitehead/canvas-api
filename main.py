75a92e36e8f26309010a5ea5d987737bcd7851fb
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-04-09 10:05:07 -0600

    Implements app.get for courses

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import os
 
 from dotenv import load_dotenv
 import requests
+from fastapi import FastAPI
+
+from models import Course
 
 # /api/v1/courses/:course_id/discussion_topics
 
 load_dotenv()
+
+app = FastAPI()
 
 access_key = os.getenv("ACCESS_TOKEN")
 
@@ -15,11 +20,11 @@ headers: dict[str, str] = {
     "Authorization": f"Bearer {access_key}"
 }
 
-response = requests.get(url=f"{base_url}/courses", headers=headers)
-r_json = response.json()
-print()
-resp: dict = {
-    "id": r_json[0]["id"],
-    "name": r_json[0]["name"]
-}
-print(resp)
+
+@app.get("/courses")
+async def get_courses() -> Course:
+    response = requests.get(url=f"{base_url}/courses", headers=headers)
+    r_json = response.json()
+
+    course = Course(id=r_json[0]["id"], name=r_json[0]["name"])
+    return course
